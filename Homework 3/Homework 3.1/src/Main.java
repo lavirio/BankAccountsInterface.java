823c0d313fc20737d0b2c2af037360216084adca
LevSukherman <convers11@me.com>
2021-11-29 21:38:40 +0300

    Version 1

---
+++ Homework 3/Homework 3.1/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int first = 1;
+        int second = 2;
+        int thirdNumber = 3;
+        int fourthNumber = 4;
+        int fifthNumber = 5;
+
+        String string = "Hello world!";
+
+        char symbolFirst = '@';
+        char symbolSecond = '*';
+
+        int integer = 10;
+
+        double number = 10.25;
+
+        System.out.print(first + "\n" + second + "\n" + thirdNumber + "\n" + fourthNumber + "\n" + fifthNumber + "\n" + string + "\n" +symbolFirst + "\n" + symbolSecond + "\n" + integer + "\n" + number);
+    }
+}
