1a3f2371ae35f858333f8ef3de405a3cfd07a6c8
LevSukherman <convers11@me.com>
2021-11-29 21:34:48 +0300

    Version 1

---
+++ Homework 1/Homework 1.2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Lev";
+        String skype = "lavir1997";
+        System.out.println("**************************");
+        System.out.println("*****    Визитка     *****");
+        System.out.println("*****    Name: " + name + "   *****");
+        System.out.println("**** Skype: " + skype + " ****");
+        System.out.println("**************************");
+    }
+}
