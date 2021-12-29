7d64ae0c2886cb4a2b27988dfd18029e008f8ecf
LevSukherman <convers11@me.com>
2021-12-29 22:10:20 +0300

    Version 2

--- Homework 37/Homework 37.1/src/main/java/Main.java
+++ Homework 37/Homework 37.1/src/main/java/Main.java
@@ -13,20 +13,22 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    private static void downloadPageFromWebsite() throws IOException {
+    private static Document downloadPageFromWebsite() throws IOException {
         String url = "https://ru.wikipedia.org/wiki/Список_станций_Московского_метрополитена";
         String html = Jsoup.parse(new URL(url), 0).html();
         List<String> website = new ArrayList<>();
         website.add(html);
         Files.write(Paths.get("src/Data/Wikipedia.html"), website);
+        return Jsoup.parse("src/Data/Wikipedia.html");
     }
 
-    private static Pattern stationName = Pattern.compile("((\\W+\\s)?)\\W+");
-    private static Pattern colorName = Pattern.compile("#\\w+");
-    private static Pattern lineNumber = Pattern.compile("^\\S+");
+    private static final Pattern stationName = Pattern.compile("((\\W+\\s)?)\\W+");
+    private static final Pattern colorName = Pattern.compile("#\\w+");
+    private static final Pattern lineNumber = Pattern.compile("^\\S+");
 
     private static Document downloadPage() throws Exception {
         String url = "https://ru.wikipedia.org/wiki/Список_станций_Московского_метрополитена";
@@ -54,7 +56,7 @@ public class Main {
     }
 
     private static String getLineNumberFromString(String lineNumberString) {
-        String number = lineNumberString.replaceAll("^0", "").trim();
+        String number = lineNumberString.replaceAll("^0", "").replaceAll("[A-я]+","");
         Matcher matcher = lineNumber.matcher(number);
         if (matcher.find()) {
             return matcher.group();
@@ -64,21 +66,29 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            List<String> stations = new ArrayList<>();
+            List<SubwayLine> lines = new ArrayList<>();
+            List<SubwayStation> stations = new ArrayList<>();
+            downloadPageFromWebsite();
             Document document = downloadPage();
 
             Element table = document.select("table").get(2);
             Elements rows = table.select("tr");
-            Elements line = document.select("div.navbox").select("table.navbox-columns-table").select("a");
-
-            line.forEach(element -> stations.add(element.text().replaceAll("\\s+","")));
 
             rows.stream().skip(1).forEach(element -> {
                 SubwayStation subwayStation = new SubwayStation(getNameFromString(element.select("a").text()), getLineNumberFromString(element.select("span.sortkey").text()));
                 SubwayLine subwayLine = new SubwayLine(element.select("span").attr("title"), getLineNumberFromString(element.select("span.sortkey").text()), getColorIdFromString(element.select("td").attr("style")));
-                
+                List<String> linesId = lines.stream().map(SubwayLine::getLineId).collect(Collectors.toList());
+                if (!linesId.contains(subwayLine.getLineId())) {
+                    lines.add(subwayLine);
+                }
+                stations.add(subwayStation);
+                lines.forEach(line -> stations.forEach(station -> {
+                    if (line.getLineId().equals(station.getLineId())) {
+                        line.addStationToLine(station);
+                    }
+                }));
             });
-
+            lines.forEach(subwayLine -> System.out.println("Линия " + subwayLine.getLineId() + " - " + subwayLine.getLineName() + " - " + subwayLine.stationCount() + " станции - " + subwayLine.getColor()));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
--- Homework 37/Homework 37.1/src/main/java/Metro/SubwayLine.java
+++ Homework 37/Homework 37.1/src/main/java/Metro/SubwayLine.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Data
 @NoArgsConstructor
@@ -12,4 +15,24 @@ public class SubwayLine {
     private String lineName;
     private String lineId;
     private String color;
+
+    List<SubwayStation> stations = new ArrayList<>();
+
+    public SubwayLine(String lineName, String lineId, String color) {
+        this.lineName = lineName;
+        this.lineId = lineId;
+        this.color = color;
     }
+
+    private int count = 0;
+
+    public void addStationToLine(SubwayStation station){
+        if (!stations.contains(station)){
+            count++;
+        stations.add(station);}
+    }
+
+    public int stationCount(){
+        return count;
+    }
+}
